e2f6a69746f403657900185c63d72d96438dd6ec
Giancarlo Ventura <khan.maytok@gmail.com>
2020-06-28 19:16:45 -0500

    :wrench: Mod configuration files

---
+++ copy_files.sh
@@ -0,0 +1,10 @@
+rm -fR /f/Mirror/Documents/Paradox\ Interactive/Crusader\ Kings\ II/mod/qqbsvqfypfzdn/**
+cp -fR common /f/Mirror/Documents/Paradox\ Interactive/Crusader\ Kings\ II/mod/qqbsvqfypfzdn
+cp -fR decisions /f/Mirror/Documents/Paradox\ Interactive/Crusader\ Kings\ II/mod/qqbsvqfypfzdn
+cp -fR events /f/Mirror/Documents/Paradox\ Interactive/Crusader\ Kings\ II/mod/qqbsvqfypfzdn
+cp -fR gfx /f/Mirror/Documents/Paradox\ Interactive/Crusader\ Kings\ II/mod/qqbsvqfypfzdn
+cp -fR history /f/Mirror/Documents/Paradox\ Interactive/Crusader\ Kings\ II/mod/qqbsvqfypfzdn
+cp -fR interface /f/Mirror/Documents/Paradox\ Interactive/Crusader\ Kings\ II/mod/qqbsvqfypfzdn
+cp -fR localisation /f/Mirror/Documents/Paradox\ Interactive/Crusader\ Kings\ II/mod/qqbsvqfypfzdn
+cp universe_eye_by_Ellysiumn /f/Mirror/Documents/Paradox\ Interactive/Crusader\ Kings\ II/mod/qqbsvqfypfzdn
+cp VERSION.txt /f/Mirror/Documents/Paradox\ Interactive/Crusader\ Kings\ II/mod/qqbsvqfypfzdn
